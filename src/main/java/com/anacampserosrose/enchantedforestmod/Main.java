7bbb72cb9cf91b12458b8cbeca1578d8b95d149d
Amanda <theearlessowl@gmail.com>
2018-10-08 00:34:21 -0700

    Add files via upload

--- src/main/java/com/anacampserosrose/enchantedforestmod/Main.java
+++ src/main/java/com/anacampserosrose/enchantedforestmod/Main.java
@@ -2,7 +2,11 @@ package com.anacampserosrose.enchantedforestmod;
 
 import com.anacampserosrose.enchantedforestmod.proxy.CommonProxy;
 import com.anacampserosrose.enchantedforestmod.util.Reference;
+import com.anacampserosrose.enchantedforestmod.worldgen.WorldGenEFOres;
+import com.anacampserosrose.enchantedforestmod.worldgen.WorldGenEFTrees;
 
+import handlers.MobDropsHandler;
+import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventHandler;
 import net.minecraftforge.fml.common.Mod.Instance;
@@ -10,6 +14,7 @@ import net.minecraftforge.fml.common.SidedProxy;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import net.minecraftforge.fml.common.registry.GameRegistry;
 
 @Mod(modid = Reference.MOD_ID, name = Reference.NAME, version = Reference.VERSION)
 public class Main {
@@ -23,6 +28,9 @@ public class Main {
 	@EventHandler
 	public static void PreInit(FMLPreInitializationEvent event)
 	{
+		GameRegistry.registerWorldGenerator(new WorldGenEFOres(), 3);
+		GameRegistry.registerWorldGenerator(new WorldGenEFTrees(), 3);
+		MinecraftForge.EVENT_BUS.register(new MobDropsHandler());
 	}
 	
 	@EventHandler
