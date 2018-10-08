d33fa444e267a9b7d4541d53f293ccc2eb0bbe30
Amanda <theearlessowl@gmail.com>
2018-10-08 00:32:44 -0700

    Add files via upload

--- src/main/java/com/anacampserosrose/enchantedforestmod/proxy/ClientProxy.java
+++ src/main/java/com/anacampserosrose/enchantedforestmod/proxy/ClientProxy.java
@@ -1,7 +1,10 @@
 package com.anacampserosrose.enchantedforestmod.proxy;
 
+import com.anacampserosrose.enchantedforestmod.util.Reference;
+
 import net.minecraft.client.renderer.block.model.ModelResourceLocation;
 import net.minecraft.item.Item;
+import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.client.model.ModelLoader;
 
 public class ClientProxy extends CommonProxy
@@ -9,5 +12,11 @@ public class ClientProxy extends CommonProxy
 	public void registerItemRenderer(Item item, int meta, String id) 
 	{
 		ModelLoader.setCustomModelResourceLocation(item, meta, new ModelResourceLocation(item.getRegistryName(), id));
+	}
+	
+	@Override
+	public void registerVariantRenderer(Item item, int meta, String filename, String id) 
+	{
+		ModelLoader.setCustomModelResourceLocation(item, meta, new ModelResourceLocation(new ResourceLocation(Reference.MOD_ID, filename), id));
 	}
 }
