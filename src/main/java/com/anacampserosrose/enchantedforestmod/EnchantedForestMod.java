629fc6d9f66ccd6ab92f354d5a464ec07a624f8f
Amanda <theearlessowl@gmail.com>
2018-10-23 19:23:03 -0700

    Add files via upload

--- src/main/java/com/anacampserosrose/enchantedforestmod/EnchantedForestMod.java
+++ src/main/java/com/anacampserosrose/enchantedforestmod/EnchantedForestMod.java
@@ -1,12 +1,30 @@
+/*******************************************************************************
+ * Credits:
+ * 
+ * Beethoven#2693:
+ * Flamingo Entity Model and Texture
+ * Hedgehog Entity Model and Texture
+ * Crystal Armor Texture
+ * 
+ * Flamingo Ambient Sound - http://sound-effects-hunter.com/free-flamingo-sounds/
+ ******************************************************************************/
+
 package com.anacampserosrose.enchantedforestmod;
 
+import com.anacampserosrose.enchantedforestmod.init.EFBiomes;
+import com.anacampserosrose.enchantedforestmod.init.ModEntities;
+import com.anacampserosrose.enchantedforestmod.init.ModLootTable;
 import com.anacampserosrose.enchantedforestmod.init.ModRecipes;
+import com.anacampserosrose.enchantedforestmod.init.ModSounds;
 import com.anacampserosrose.enchantedforestmod.proxy.CommonProxy;
+import com.anacampserosrose.enchantedforestmod.tabs.EnchantedForestTab;
 import com.anacampserosrose.enchantedforestmod.util.Reference;
 import com.anacampserosrose.enchantedforestmod.util.handlers.MobDropsHandler;
-import com.anacampserosrose.enchantedforestmod.worldgen.WorldGenEFOres;
-import com.anacampserosrose.enchantedforestmod.worldgen.WorldGenEFTrees;
+import com.anacampserosrose.enchantedforestmod.util.handlers.RenderHandler;
+import com.anacampserosrose.enchantedforestmod.world.WorldGenEFOres;
+import com.anacampserosrose.enchantedforestmod.world.WorldGenEFTrees;
 
+import net.minecraft.creativetab.CreativeTabs;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventHandler;
@@ -23,6 +41,8 @@ public class EnchantedForestMod {
 	@Instance
 	public static EnchantedForestMod instance;
 	
+	public static final CreativeTabs enchantedforesttab = new EnchantedForestTab("enchantedforesttab");
+	
 	@SidedProxy(clientSide = Reference.CLIENT_PROXY_CLASS, serverSide = Reference.COMMON_PROXY_CLASS)
 	public static CommonProxy proxy;
 	
@@ -31,6 +51,12 @@ public class EnchantedForestMod {
 	{
 		GameRegistry.registerWorldGenerator(new WorldGenEFOres(), 3);
 		GameRegistry.registerWorldGenerator(new WorldGenEFTrees(), 3);
+		
+		EFBiomes.registerBiomes();
+		
+		ModEntities.registerEntities();
+		RenderHandler.registerEntityRenders();
+	
 		MinecraftForge.EVENT_BUS.register(new MobDropsHandler());
 	}
 	
@@ -38,11 +64,14 @@ public class EnchantedForestMod {
 	public static void init(FMLInitializationEvent event)
 	{
 		ModRecipes.init();
+		ModLootTable.init();
+		ModSounds.init();
 	}
 	
 	@EventHandler
 	public static void Postinit(FMLPostInitializationEvent event)
 	{
 	}
+	
 
 }
