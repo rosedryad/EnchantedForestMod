d51ffb0b6eda06fecb12c956766ff78df56c4e6a
Amanda <theearlessowl@gmail.com>
2018-10-23 19:23:43 -0700

    flamingo entity

--- src/main/java/com/anacampserosrose/enchantedforestmod/util/Reference.java
+++ src/main/java/com/anacampserosrose/enchantedforestmod/util/Reference.java
@@ -9,4 +9,6 @@ public class Reference {
 	public static final String CLIENT_PROXY_CLASS = "com.anacampserosrose.enchantedforestmod.proxy.ClientProxy";
 	public static final String COMMON_PROXY_CLASS = "com.anacampserosrose.enchantedforestmod.proxy.CommonProxy";
 	
+	public static final int ENTITY_FLAMINGO = 120;
+	
 }
