f5ca4892ed49639841c67547521b7421ed630593
Amanda <theearlessowl@gmail.com>
2020-04-11 16:53:44 -0700

    Add files via upload

--- src/main/java/com/anacampserosrose/enchantedforestmod/items/armor/ArmorBase.java
+++ src/main/java/com/anacampserosrose/enchantedforestmod/items/armor/ArmorBase.java
@@ -2,12 +2,11 @@ package com.anacampserosrose.enchantedforestmod.items.armor;
 
 import com.anacampserosrose.enchantedforestmod.EnchantedForestMod;
 import com.anacampserosrose.enchantedforestmod.init.ModItems;
-import com.anacampserosrose.enchantedforestmod.util.IHasModel;
 
 import net.minecraft.inventory.EntityEquipmentSlot;
 import net.minecraft.item.ItemArmor;
 
-public class ArmorBase extends ItemArmor implements IHasModel {
+public class ArmorBase extends ItemArmor {
 
 	public ArmorBase(String name, ArmorMaterial materialIn, int renderIndexIn, EntityEquipmentSlot equipmentSlotIn) {
 		super(materialIn, renderIndexIn, equipmentSlotIn);
@@ -18,9 +17,4 @@ public class ArmorBase extends ItemArmor implements IHasModel {
 		ModItems.ITEMS.add(this);
 	}
 	
-	@Override
-	public void registerModels() 
-	{
-		EnchantedForestMod.proxy.registerItemRenderer(this, 0, "inventory");
-	}
 }
